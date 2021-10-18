6befaec778057715779f08a9e9101add9e2298bf
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2021-10-19 01:30:08 +0300

    updated

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,13 +1,11 @@
 import '../sass/_common.scss';
 import '../sass/_promisification.scss';
-
 /*
     ### 1
 Напиши функцию `delay(ms)`, которая возвращает промис, 
 переходящий в состояние `"resolved"` через `ms` миллисекунд. 
 Значением исполнившегося промиса должно быть то кол-во миллисекунд 
 которое передали во время вызова функции `delay`.
-
 const delay = ms => {
     // Твой код
 };
@@ -18,8 +16,7 @@ delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
 */
 
-    // ======== Answer ========
-
+//    ======== Answer ========
 const delay = ms => {
     return new Promise (resolve => {
         setTimeout(() => {
@@ -27,9 +24,58 @@ const delay = ms => {
         }, ms)
     });
 };
-      
+    
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
+
+
+/* ==================================================================================
+ * Создание промиса
+ *  - Класс Promise
+ *  - resolve
+ *  - reject
+ *  - Promise.prototype.then(onResolve, onReject)
+ */
+const promise = new Promise((resolve, reject) => {
+    const canFullfill = Math.random() > 0.5;
+    
+    setTimeout(() => {
+        if (canFullfill) {
+            resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+        }
+
+        reject('Промис выполнился с ошибкой (отклонён, rejected)');
+    }, 1000);
+});
+
+  // promise.then(onFulfilled, onRejected);
+function onFullfilled(result) {
+    console.log('onFulfilled -> onFulfilled');
+    console.log(`✅ ${result}`);
+};
+
+function onRejected(error) {
+    console.log('onRejected -> onRejected');
+    console.log(`❌ ${error}`);
+};
+
+/*
+ * Цепочки промисов (chaining)
+ * Promise.prototype.catch(error)
+ * Promise.prototype.finally()
+ */
+promise
+.then(onFullfilled)
+.then(x=>{
+    console.log(x);
+
+    return 10;
+})
+.then(y=>{
+    console.log(y);
+})
+.catch(onRejected)
+.finally(() => console.log('Я буду выполнен в любом случае'));
--- src/js/task-2.js
+++ src/js/task-2.js
@@ -5,7 +5,6 @@ import '../sass/_promisification.scss';
     ### 2
 Перепиши функцию `toggleUserState()` так, чтобы она не использовала callback-функцию callback, 
 а принимала всего два параметра `allUsers` и `userName` и возвращала промис.
-
 const users = [
   { name: 'Mango', active: true },
   { name: 'Poly', active: false },
@@ -19,21 +18,18 @@ const toggleUserState = (allUsers, userName, callback) => {
   callback(updatedUsers);
 };
 const logger = updatedUsers => console.table(updatedUsers);
-
     // Сейчас работает так
 toggleUserState(users, 'Mango', logger);
 toggleUserState(users, 'Lux', logger);
 */
 
     // ======== Answer ========
-
 const users = [
     { name: 'Mango', active: true },
     { name: 'Poly', active: false },
     { name: 'Ajax', active: true },
     { name: 'Lux', active: false },
 ];
-
 const toggleUserState = (allUsers, userName) => {
     const updatedUsers = allUsers
     .map(user => 
@@ -50,3 +46,80 @@ const logger = updatedUsers => console.table(updatedUsers);
 
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
+
+
+/* ===================================
+ * Промисификация:
+ * - Поблема доступа к результату промиса с колбеком
+ * - Функция которая возвращает промис
+ */
+// const makeOrder = dish => {
+//   const DELAY = 1000;
+
+//   return new Promise((resolve, reject) => {
+//     const passed = Math.random() > 0.5;
+
+//     setTimeout(() => {
+//       if (passed) {
+//         resolve(`✅ Вот ваш заказ: ${dish}`);
+//       }
+
+//       reject('❌ Упс, у нас закончились продукты');
+//     }, DELAY);
+//   });
+// };
+
+// makeOrder('пирожок').then(onMakeOrderSuccess).catch(onMakeOrderError);
+
+// function onMakeOrderSuccess(result) {
+//   console.log('onMakeOrderSuccess');
+//   console.log(result);
+// }
+
+// function onMakeOrderError(error) {
+//   console.log('onMakeOrderError');
+//   console.log(error);
+// }
+
+/*
+ * Покемоны с https://pokeapi.co/
+ */
+const fetchPokemonById = id => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r => r.json());
+};
+
+fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
+  
+fetchPokemonById(2).then(onFetchSuccess).catch(onFetchError);
+
+fetchPokemonById(3).then(onFetchSuccess).catch(onFetchError);
+
+function onFetchSuccess(pokemon) {
+  console.log('onFetchSuccess -> onFetchSuccess');
+  console.log(pokemon);
+}
+
+function onFetchError(error) {
+  console.log('onFetchError -> onFetchError');
+  console.log('Это в блоке catch');
+  console.log(error);
+}
+
+// makePromise
+const makePromise = () => {
+  return new Promise((resolve, reject) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        resolve('✅ Куку это resolve');
+      }
+
+      reject('❌ все пропало это reject');
+    }, 2000);
+  });
+};
+
+makePromise()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
