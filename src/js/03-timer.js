217cf3928fcd0d2af7d0484717a7b7e601dfe16a
DimaLitvinenko <dimaly.too@gmail.com>
2022-10-07 18:23:21 +0300

    styles changes

--- src/js/03-timer.js
+++ src/js/03-timer.js
@@ -32,87 +32,82 @@ import '../sass/_timer.scss';
 //   };
 // }, 1000);
 
-
 // ======== СПОСОБ №2 =========
 class CountdownTimer {
-  constructor({ selector, targetDate }) {
-    this.intervalId = null;
-    this.selector = selector;
-    this.targetDate = targetDate;
-    this.start();
+    constructor({ selector, targetDate }) {
+        this.intervalId = null;
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.start();
 
-    this.refs = {
-      days: document.querySelector(
-        `${this.selector} span[data-value="days"]`
-      ),
-      hours: document.querySelector(
-        `${this.selector} span[data-value="hours"]`
-      ),
-      mins: document.querySelector(
-        `${this.selector} span[data-value="mins"]`
-      ),
-      secs: document.querySelector(
-        `${this.selector} span[data-value="secs"]`
-      ),
+        this.refs = {
+            days: document.querySelector(
+                `${this.selector} span[data-value="days"]`,
+            ),
+            hours: document.querySelector(
+                `${this.selector} span[data-value="hours"]`,
+            ),
+            mins: document.querySelector(
+                `${this.selector} span[data-value="mins"]`,
+            ),
+            secs: document.querySelector(
+                `${this.selector} span[data-value="secs"]`,
+            ),
+        };
+    }
 
-    };
-  }
-  
-  calcTheTime() {
-    let dateNow = Date.now();
-    let deltaTime = this.targetDate - dateNow;
+    calcTheTime() {
+        let dateNow = Date.now();
+        let deltaTime = this.targetDate - dateNow;
 
-    this.timerReview(deltaTime);   // ???
+        this.timerReview(deltaTime); // ???
 
-    this.getTimeComponents(deltaTime);
-  }
+        this.getTimeComponents(deltaTime);
+    }
 
-  // Проверка на время  
-  timerReview(time) {   // ???
-    if (time < 0) {
-      document.querySelector(`${this.selector}`).innerHTML = "The countdown is over!";
+    // Проверка на время
+    timerReview(time) {
+        // ???
+        if (time < 0) {
+            document.querySelector(`${this.selector}`).innerHTML =
+                'The countdown is over!';
 
-      clearInterval(intervalId);
-    };
-  }
+            clearInterval(intervalId);
+        }
+    }
 
-  getTimeComponents(time) {
-    let days = this.pad(
-      Math.floor(time / (1000 * 60 * 60 * 24))
-    );
-    let hours = this.pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    );
-    let mins = this.pad(
-      Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
-    );
-    let secs = this.pad(
-      Math.floor((time % (1000 * 60)) / 1000)
-    );
-      
-    this.updateComponents(days, hours, mins, secs);
-  }
-  
-  updateComponents(days, hours, mins, secs) {  
-    this.refs.days.textContent = days; 
-    this.refs.hours.textContent = hours;
-    this.refs.mins.textContent = mins;
-    this.refs.secs.textContent = secs;
-  }
+    getTimeComponents(time) {
+        let days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        let hours = this.pad(
+            Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        );
+        let mins = this.pad(
+            Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+        );
+        let secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-  pad(value) {
-    return String(value).padStart(2, '0');
-  }
+        this.updateComponents(days, hours, mins, secs);
+    }
 
-  start() {
-    this.intervalId = setInterval(() => { 
-      this.calcTheTime();
-    }, 1000); 
-  }
+    updateComponents(days, hours, mins, secs) {
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = hours;
+        this.refs.mins.textContent = mins;
+        this.refs.secs.textContent = secs;
+    }
 
-};
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
 
-const timeToMyBithday = new CountdownTimer ({
-  selector: '#timer-1',
-  targetDate: new Date('Apr 23, 2022'),
+    start() {
+        this.intervalId = setInterval(() => {
+            this.calcTheTime();
+        }, 1000);
+    }
+}
+
+const timeToMyBithday = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Apr 23, 2023'),
 });
--- src/js/notification.js
+++ src/js/notification.js
@@ -3,26 +3,25 @@ import '../sass/_notification.scss';
 const NOTIFICATION_DELAY = 5000;
 let timeoutId = null;
 const notification = document.querySelector('.js-alert');
- 
+
 notification.addEventListener('click', notificationClickHandler);
- 
+
 showNotification();
- 
+
 function notificationClickHandler() {
-  hideNotification();
-  clearTimeout(timeoutId);
-}
- 
-function showNotification() {
-  notification.classList.add('is_active');
- 
-  timeoutId = setTimeout(() => {
-    console.log('Закрываем алерт автоматически');
     hideNotification();
-  }, NOTIFICATION_DELAY);
+    clearTimeout(timeoutId);
 }
- 
+
+function showNotification() {
+    notification.classList.add('is_active');
+
+    timeoutId = setTimeout(() => {
+        // console.log('Закрываем алерт автоматически');
+        hideNotification();
+    }, NOTIFICATION_DELAY);
+}
+
 function hideNotification() {
-  notification.classList.remove('is_active');
+    notification.classList.remove('is_active');
 }
- 
