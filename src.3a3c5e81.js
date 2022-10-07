46e8aadfe728c0e00ca544371d1a00dd6f83da6f
DimaLitvinenko <80059894+DimaLitvinenko@users.noreply.github.com>
2022-10-07 15:24:32 +0000

    Deploying to gh-pages from @ DimaLitvinenko/goit-Js-hw-11@217cf3928fcd0d2af7d0484717a7b7e601dfe16a 🚀

--- src.3a3c5e81.js
+++ src.3a3c5e81.js
@@ -1,10 +1,10 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
 },{}],"SiMs":[function(require,module,exports) {
-"use strict";require("../sass/_notification.scss");const e=5e3;let s=null;const t=document.querySelector(".js-alert");function c(){n(),clearTimeout(s)}function i(){t.classList.add("is_active"),s=setTimeout(()=>{console.log("Закрываем алерт автоматически"),n()},e)}function n(){t.classList.remove("is_active")}t.addEventListener("click",c),i();
+"use strict";require("../sass/_notification.scss");const t=5e3;let e=null;const s=document.querySelector(".js-alert");function i(){n(),clearTimeout(e)}function c(){s.classList.add("is_active"),e=setTimeout(()=>{n()},t)}function n(){s.classList.remove("is_active")}s.addEventListener("click",i),c();
 },{"../sass/_notification.scss":"clu1"}],"VyiV":[function(require,module,exports) {
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var t={startBtn:document.querySelector('button[data-action="start"]'),stopBtn:document.querySelector('button[data-action="stop"]'),resetBtn:document.querySelector(".btn_reset"),body:document.querySelector("#js_body")};exports.default=t;
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/notification.js"),require("./js/refs.js");
 },{"./sass/main.scss":"clu1","./js/notification.js":"SiMs","./js/refs.js":"VyiV"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-Js-hw-11/src.87dc3589.js.map
+//# sourceMappingURL=/goit-Js-hw-11/src.3a3c5e81.js.map
